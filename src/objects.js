b81a293ac23bca12730f13dbc20a6ab8dc5d0d75
fitzsimk <kfitzsimmons11@gmail.com>
2015-04-20 19:25:04 +0000

    add constructor with functions for message log

--- src/objects.js
+++ src/objects.js
@@ -1,60 +1,108 @@
-/**
-* Create an object literal with the following key value pairs:
-* type: {string} 'Goldfish'
-* brand: {string} 'Pepperidge Farm'
-* flavor: {string} 'Cheddar'
-* count: {number} 2000
-* It should be returned directly by the following function
-* @return {object} - the object literal
-*/
+
+/** started 11:35AM
+ * Create an object literal with the following key value pairs:
+ * type: {string} 'Goldfish'
+ * brand: {string} 'Pepperidge Farm'
+ * flavor: {string} 'Cheddar'
+ * count: {number} 2000
+ * It should be returned directly by the following function
+ * @return {object} - the object literal
+ */
 
 function returnObjectLiteral() {
-  //your code here
-  return undefined; //Modify ONLY this line
-  //end your code
+	//your code here
+	return {
+		type : "Goldfish",
+		brand : "Pepperidge Farm",
+		flavor : "Cheddar",
+		count : 2000
+	}; //Modify ONLY this line
+	//end your code
 }
 
 /**
-* Create a constructor function for a `MessageLog` object.
-* @constructor
-* @param {string} user - The user associated to the message log
-* The string indicating the user should be stored in the user property of the
-* object instances.
-*
-* In addition, the following methods should be
-* callable on a MessageLog object:
-* logMessage( {string} messageText, {number} direction) - This should log a
-* message
-* as either being sent or received. A direction of 0 indicates it is a message
-* the user sent. A direction of 1 indicates it is a message the user received.
-* Behavior for other numbers is undefined.
-* getSentMessage({number} n) - returns as a string, the content of the nth most
-* recently sent message. To conserve memory, the object should only keep the
-* last 5 message. n=0 retrieves the most recent n=4 retrieves the least recent
-* of the 5.
-* totalSent() - returns an integer indicating the total number of messages sent
-* totalReceived() - returns an integer indicating the total number of messages
-* received
-*/
+ * Create a constructor function for a `MessageLog` object.
+ * @constructor
+ * @param {string} user - The user associated to the message log
+ * The string indicating the user should be stored in the user property of the
+ * object instances.
+ *
+ * In addition, the following methods should be
+ * callable on a MessageLog object:
+ * logMessage( {string} messageText, {number} direction) - This should log a
+ * message as either being sent or received. A direction of 0 indicates it is a message
+ * the user sent. A direction of 1 indicates it is a message the user received.
+ * Behavior for other numbers is undefined.
+ * getSentMessage({number} n) - returns as a string, the content of the nth most
+ * recently sent message. To conserve memory, the object should only keep the
+ * last 5 message. n=0 retrieves the most recent n=4 retrieves the least recent
+ * of the 5.
+ * totalSent() - returns an integer indicating the total number of messages sent
+ * totalReceived() - returns an integer indicating the total number of messages
+ * received
+ */
+
+function createMessageLog(user) {
+
+	this.user = user;
+	this.sent = [];
+	this.received = [];
+
+	this.logMessage = function (messageText, direction) {
+
+		if (direction === 0) {
+			if (this.sent.length === 5) {
+				this.sent.shift();
+			}
+			this.sent.push(messageText);
+			this.tSent++;
+		} else if (direction === 1) {
+			if (this.received.length === 5) {
+				this.received.shift();
+			}
+			this.received.push(messageText);
+			this.tReceived++;
+		}
+	};
+
+	this.getSentMessage = function (n) {
+
+		return this.sent[n];
+
+	};
+	
+	this.totalSent = function () {
+
+		return this.tSent;
+	};
+
+	this.totalReceived = function () {
+
+		return this.tReceived;
+	};
+}
+
+//var rob = createMessageLog("Robert Pierce");
+
 
 //your code here
 
 //end your code
 
 /**
-* Add a method to the MessageLog prototype:
-* lastReceivedMessage() - returns the message text of the last message the user
-* received.
-*/
+ * Add a method to the MessageLog prototype:
+ * lastReceivedMessage() - returns the message text of the last message the user
+ * received.
+ */
 //your code here
 
 //end your code
 
 /**
-* Create an instance of a `MessageLog` for the user "BlackHatGuy". Have the
-* instance receive 3 messages: "foo", "bar" and "baz", received in that order.
-* Assign it to the variable myLog.
-*/
+ * Create an instance of a `MessageLog` for the user "BlackHatGuy". Have the
+ * instance receive 3 messages: "foo", "bar" and "baz", received in that order.
+ * Assign it to the variable myLog.
+ */
 
 //your code here
 
